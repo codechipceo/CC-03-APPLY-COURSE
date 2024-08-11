09cf938f0aa299ce1bffa92cba83537690420d85
Mansab Mir <mansabmirsde@gmail.com>
2024-08-11 14:10:26 +0530

    dahshboard build

---
+++ dashboard/dashboard-server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from 'dotenv'
+const { config} = dotenv
+
+config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+const port = process.env.PORT || 4000;
+console.log(port)
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
